5d6f658be471e74218561c8c903101df233513fc
TWaltze <tylerwaltze@gmail.com>
2014-05-15 01:28:28 -0700

    Allow for cross-origin requests via xhr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :null_session
 
+	after_filter :set_access_control_headers
+
 	rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
 	private
+		def set_access_control_headers
+			headers['Access-Control-Allow-Origin'] = '*'
+			headers['Access-Control-Request-Method'] = '*'
+		end
+
 		def record_not_found(error)
 			render :json => {:error => error.message}, :status => :not_found
 		end
